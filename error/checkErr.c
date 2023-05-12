518473ffffd598a92b5a4e5e452d229ee45e1164
Qwerty7310 <daniyl.2004@yandex.ru>
2023-05-12 18:15:02 +0300

    ErrorTest

--- error/checkErr.c
+++ error/checkErr.c
@@ -11,31 +11,31 @@ int CheckErr(int code)
         {
             case -1:
             {
-                char error[] = "\nYou entered wrong difficulty.\n";
+                char error[] = "You entered wrong difficulty.";
                 PrintError(error);
                 return 1;
             }
             case -2:
             {
-                char error[] = "\nError while printing field.\nExploding your PC..\n";
+                char error[] = "Error while printing field. Exploding your PC..";
                 PrintError(error);
                 return -2;
             }
             case -3:
             {
-                char error[] = "\nYou entered coordinates outside the field.\n";
+                char error[] = "You entered coordinates outside the field.";
                 PrintError(error);
                 return 1;
             }
             case -4:
             {
-                char error[] = "\nYou entered incorrect symbol.\n";
+                char error[] = "You entered incorrect symbol.";
                 PrintError(error);
                 return 1;
             }
             default:
             {
-                char error[] = "Unexpected error!\n";
+                char error[] = "Unexpected error!";
                 PrintError(error);
                 return -999;
             }
--- error/errorTest/errorTest.c
+++ error/errorTest/errorTest.c
@@ -2,35 +2,41 @@
 #include <stdio.h>
 #include <string.h>
 
-char arr[100] = {};
 
-inline void PrintError(char arr1[]) {
-    strcpy(arr, arr1);
-}
+//inline void PrintError(char *arr1) {
+//    strcpy(arr, arr1);
+//}
 
 int TestCheckErr(char msg[]) {
 
     struct ErrorText errors[6] = {
             {0, ""},
-            {-1, "\nYou entered wrong difficulty.\n"},
-            {-2, "\nError while printing field.\nExploding your PC..\n"},
-            {-3, "\nYou entered coordinates outside the field.\n"},
-            {-4, "\nYou entered incorrect symbol.\n"},
-            {-5, "Unexpected error!\n"},
+            {-1, "\fYou entered wrong difficulty."},
+            {-2, "\fError while printing field. Exploding your PC.."},
+            {-3, "\fYou entered coordinates outside the field."},
+            {-4, "\fYou entered incorrect symbol."},
+            {-5, "\fUnexpected error!"},
     };
 
 
     if (CheckErr(0)) {
-        sprintf(msg, "TestCheckErr: checkErr returned %d; expected 0\n", CheckErr(0));
+        sprintf(msg, "TestCheckErr: checkErr returned %d; expected 0", CheckErr(0));
         return 1;
     }
-
     for (int i = 1; i < 6; i++) {
+        freopen("for_errorTest.txt","w+",stdout);
         CheckErr(errors[i].code);
+        freopen("CON","w",stdout);
+        char arr[100] = {};
+        FILE *file_res;
+        FILE *file_exp;
+        file_res = fopen("for_errorTest.txt", "r");
+        fgets(arr, 100, file_res);
         if (strcmp(arr, errors[i].text) != 0) {
-            sprintf(msg, "TestCheckErr: checkErr returned %s; expected %s\n", arr, errors[i].text);
+            sprintf(msg, "TestCheckErr: checkErr returned <%s>; expected <%s>\n", arr, errors[i].text);
             return 1;
         }
+        fclose(file_res);
     }
 
     return 0;
